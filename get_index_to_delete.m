f29d681bb2e1a9e75517a9600cef2eeb48cf6cb6
Guillaume Brizolier <gbrizolier@gmail.com>
2020-02-28 15:15:09 +0100

    fix return from handle

--- get_index_to_delete.m
+++ get_index_to_delete.m
@@ -1,10 +1,10 @@
-function X = forum_handle2()
+function indices = get_index_to_delete()
     [~,~,DATAF]=Average5(600,1);
-    for i = 0:29
+    for i = 5:6
         m=DATAF(1+i*30:30+i*30,2:end)';
         e = eps(max(m(:)));
         figure,
-        boxplot( DATAF(1+i*30:30+i*30,2:end)','whisker',3 )
+        boxplot(max(m),'whisker',3 )
         h = flipud(findobj(gcf,'tag','Outliers')); % flip order of handles
         for jj = 1 : length( h )
             x =  get( h(jj), 'XData' );
@@ -13,9 +13,8 @@ function X = forum_handle2()
                 if not( isnan( x(ii) ) )
                     ix = find( abs( m(:,jj)-y(ii) ) < e );
                     text( x(ii), y(ii), sprintf( '\\leftarrowY%02d', ix ) )
-                    X = [X,ix];
                 end
             end
-        end  
-    end    
+        end
+    end
 end
