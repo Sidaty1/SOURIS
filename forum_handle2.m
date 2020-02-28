23d03e98fc52f070cf8c66d3b5edbf54be995b08
laurine97 <devillers.laurine@gmail.com>
2020-02-28 15:13:43 +0100

    wtf

--- forum_handle2.m
+++ forum_handle2.m
@@ -1,4 +1,10 @@
 function h = forum_handle2()
+    [~,~,DATAF]=Average5(600,1);
+    for i = 0:29
+        m=DATAF(1+i*30:30+i*30,2:end)';
+        e = eps(max(m(:)));
+        figure,
+        boxplot( DATAF(1+i*30:30+i*30,2:end)','whisker',3 )
         h = flipud(findobj(gcf,'tag','Outliers')); % flip order of handles
         for jj = 1 : length( h )
             x =  get( h(jj), 'XData' );
