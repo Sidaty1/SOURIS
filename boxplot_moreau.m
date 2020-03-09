52b1c503a35d8a230b4f1e529da45749111ccd4b
ArturoM <arturo_mollinedo@hotmail.com>
2020-03-09 11:38:26 +0100

    bloxplot moreau

--- boxplot_moreau.m
+++ boxplot_moreau.m
@@ -1,12 +1,14 @@
 function [m,X] = boxplot_moreau()
     [time,~,DATAF]=Average5(600,1);
     X=[];
-    limtimeinf=5;
-    limtimesup=6;
-    m=DATAF(find(time(time>limtimeinf & time<limtimesup)),2:end)';
+    limtimeinf=5.0;
+    limtimesup=6.0;
+    [~,indtimeinf]=min(abs(time-limtimeinf));
+    [~,indtimesup]=min(abs(time-limtimesup));
+    m=DATAF(indtimeinf:indtimesup,2:end)';
     e = eps(max(m(:)));
     figure,
-    boxplot(m,'whisker',3)
+    boxplot(m,'whisker',2)
     h = flipud(findobj(gcf,'tag','Outliers')); % flip order of handles
     for jj = 1 : length( h )
         x =  get( h(jj), 'XData' );
@@ -19,4 +21,5 @@ function [m,X] = boxplot_moreau()
             end
         end
     end
+    X=unique(X);
 end
