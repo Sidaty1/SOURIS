9947ed542b92ef3d73645c4b367ed166023b7f92
ArturoM <arturo_mollinedo@hotmail.com>
2020-03-09 13:53:39 +0100

    adding boxplot_nous

--- boxplot_nous.m
+++ boxplot_nous.m
@@ -1,14 +1,14 @@
-function [m,X] = boxplot_moreau()
+function [m,X] = boxplot_nous()
     [time,~,DATAF]=Average5(600,1);
     X=[]; % array containing the mabels of outliers
     limtimeinf=5.0; %lower limit of stimulation time window
     limtimesup=6.0; %higher limit of stimulation time window
     [~,indtimeinf]=min(abs(time-limtimeinf)); % finding the index corresponding to limtimeinf
     [~,indtimesup]=min(abs(time-limtimesup)); % finding the index corresponding to limtimesup
-    m=max(DATAF(indtimeinf:indtimesup,2:end))'; % Maximum of each label in that interval
+    m=DATAF(indtimeinf:indtimesup,2:end)'; % transposing DATA matrix to use boxplot
     e = eps(max(m(:))); %eps for precision limit
     figure,
-    boxplot(m,'whisker',1.5) %whisker length determined to have 10-20% outliers
+    boxplot(m,'whisker',1.5) %whisker length determined to have 
     h = flipud(findobj(gcf,'tag','Outliers')); % flip order of handles
     for jj = 1 : length( h )
         x =  get( h(jj), 'XData' );
